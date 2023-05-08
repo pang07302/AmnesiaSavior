4e4438a6c3f2fbc3387889eec4854aca3c9ee3ef
Chi Fong Pang <73105555+pang07302@users.noreply.github.com>
2023-05-08 15:42:59 +0800

    adjust topbar layout #1

--- src/components/TopBar.js
+++ src/components/TopBar.js
@@ -1,68 +1,52 @@
-import React from 'react';
-import { StyleSheet, View, Text, Pressable} from 'react-native';
-import FontAwesome from 'react-native-vector-icons/FontAwesome';
-import {useFonts} from 'expo-font';
+import React from "react";
+import { StyleSheet, View, Text, Pressable } from "react-native";
+import FontAwesome from "react-native-vector-icons/FontAwesome";
+import { useFonts } from "expo-font";
 
-const TopBar = ({title, onPress}) => {
-    const [loaded] = useFonts({
-        'Roboto-Medium': require('./../../assets/fonts/Roboto-Medium.ttf')
-     })
- 
-     if(!loaded){
-         return null;
-     }
-    return (
-        <View style={styles.container}> 
-            <View>
-                <Pressable onPress={onPress} style={styles.iconContainer}>
-                    <FontAwesome name="angle-left" size={36} color="black" />
-                </Pressable>
-            </View> 
-            <View style={styles.textContainer}>
-                <Text style={styles.text}>{title}</Text>
-            </View>
-            
-        </View>
-    )
+const TopBar = ({ title, onPress }) => {
+  const [loaded] = useFonts({
+    "Roboto-Medium": require("./../../assets/fonts/Roboto-Medium.ttf"),
+  });
+
+  if (!loaded) {
+    return null;
+  }
+  return (
+    <View style={styles.container}>
+      <Pressable onPress={onPress} style={styles.iconContainer}>
+        <FontAwesome name="angle-left" size={36} color="black" />
+      </Pressable>
+
+      <View style={styles.textContainer}>
+        <Text style={styles.text}>{title}</Text>
+      </View>
+    </View>
+  );
 };
 
 const styles = StyleSheet.create({
-    container: {
-        backgroundColor:'blue',
-        flexDirection: 'row',
-        //borderWidth:2,
-        borderColor: 'red',
-        marginTop:50,
-        height: 40,
-
-    },
-    iconContainer: {
-
-        position: 'fixed',
-        display:'flex',
-        zIndex: -1,
-        height: '100%',
-        backgroundColor:'purple'
-       
-
-        
-    },
-    textContainer: {
-  
-      
-        justifyContent: 'center',
-        flexGrow: 1,
-        alignItems: 'center',
-        backgroundColor:'green'
-        
-    },
-    text: {
-        fontSize: 24,
-        fontFamily: 'Roboto-Medium',
-    }
-
-
-
-})
+  container: {
+    position: "relative",
+    height: 40,
+    flexDirection: "row",
+    marginTop: 56,
+    backgroundColor: "#00ffff", //delete
+  },
+  iconContainer: {
+    position: "absolute",
+    height: "100%",
+    paddingHorizontal: 16,
+    zIndex: 1,
+  },
+  textContainer: {
+    alignItems: "center",
+    justifyContent: "center",
+    flexGrow: 1,
+  },
+  text: {
+    fontSize: 24,
+    fontFamily: "Roboto-Medium",
+  },
+});
 
 export default TopBar;
